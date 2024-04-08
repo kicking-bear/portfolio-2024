a71fac5e7cbcd8c326ce32a9f754e13f7b718e3e
kicking-bear <danielmanning94@gmail.com>
2024-04-08 13:01:26 -0400

    Initial commit

---
+++ src/components/Main.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-1 gap-10 py-8  sm:py-14">
+        <div class="flex flex-col lg:justify-center text-center lg:tex-left gap-6 md:gap-8 lg:gap-10">
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi! I'm <span class="Rubik text-violet-400">Daniel</span> Manning <br/> UX/UI <span class="Rubik text-violet-400"> Designer </span>
+            </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                My <span class="text-violet-400">favorite tech</span> includes Javascript (Nxt.JS or SvelteKit), TailwindCSS, Node.js + UX/UI Designer
+            </p>
+            <a class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl Rubik relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950">
+                <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
+                </div>
+                <h4 class="relative z-9">Get in touch</h4>
+            </a>
+    </div>
+    </section>
+</main>
