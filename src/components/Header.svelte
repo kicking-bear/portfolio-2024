e66e0d58ff4824a4f6851f0ed7d7140b8d806311
Daniel Manning <danielmanning@janices-mbp.mynetworksettings.com>
2024-04-09 10:16:53 -0400

    Macbook commit

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -9,7 +9,7 @@
 
 </script>
 
-<header class={'sticky - z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid' + (y > 0 ? 'py-4 bg-slate-950 boerder-violet-950' : 'py-6 bg-transparent border-transparent')}>
+<header class={'sticky - z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid' + (y > 0 ? 'py-4 bg-slate-950 border-violet-950' : 'py-6 bg-transparent border-transparent')}>
     <h1 class="font-medium">
         <b class="font-bold Rubik">Daniel</b> Manning
 
@@ -24,7 +24,7 @@
     <a 
         href="#"
         target="_blank"
-        class="bludShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
+        class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
         <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
         </div>
         <h4 class="relative z-9">Get in touch</h4>
