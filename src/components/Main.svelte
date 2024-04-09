6790b64d584c58227ee1c65bfce73e099ee17c17
kicking-bear <danielmanning94@gmail.com>
2024-04-09 17:39:52 -0400

    Updating copy

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -9,16 +9,16 @@
                 What are you looking for?
             </h2>
             <p class="text-base sm:text-lg md:text-xl">
-                My <span class="text-violet-400">favorite tech</span> includes Javascript (Nxt.JS or SvelteKit), TailwindCSS, Node.js + UX/UI Designer
+                I'm a <span class="text-violet-400">designer</span> who loves technology. You can find me coding using TailwindCSS, prototyping in figma, and blank on the weekdays. 3D modeling, printing, illustrating on the weekend.
             </p>
-            <div class="flex flex-col lg:flex-row justify-center lg:justify-start text-center lg:text-center gap-6 md:gap-8 lg:gap-10">
-                <a class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl Rubik relative overflow-hidden px-6 py-3 group rounded-full bg-slate-950 text-white">
-                    <div class="absolute top-0 right-full w-full h-full bg-slate-200 opacity-20 group-hover:translate-x-full z-0 duration-200">
+            <div class="flex flex-col mx-auto md:flex-row justify-center lg:justify-start text-center lg:text-center gap-6 md:gap-8 lg:gap-10">
+                <a class="blueShadow lg:mr-auto text-base sm:text-lg md:text-xl Rubik relative overflow-hidden px-6 py-3 group rounded-full bg-slate-950 text-white cursor-default">
+                    <div class="absolute top-0 right-full w-full h-full">
                     </div>
                     <h4 class="relative z">UX/UI</h4>
                 </a>
-                <a class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl Rubik relative overflow-hidden px-6 py-3 group rounded-full bg-slate-950 text-white">
-                    <div class="absolute top-0 right-full w-full h-full bg-slate-200 opacity-20 group-hover:translate-x-full z-0 duration-200">
+                <a class="blueShadow lg:mr-auto text-base sm:text-lg md:text-lg Rubik relative overflow-hidden px-6 py-3 group rounded-full bg-slate-100 text-black cursor-pointer">
+                    <div class="absolute top-0 right-full w-full h-full bg-slate-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
                     </div>
                     <h4 class="relative z">Graphic Design</h4>
                 </a>
