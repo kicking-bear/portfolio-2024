3c31d9e46cc48176f1482bf1cc2efcc161854443
kicking-bear <danielmanning94@gmail.com>
2024-05-07 11:06:46 -0400

    Addition of contact form

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -11,33 +11,117 @@
             <p class="text-base sm:text-lg md:text-xl">
                 I'm a <span class="text-violet-400">designer</span> who loves technology. You can find me coding using TailwindCSS, prototyping in figma, and blank on the weekdays. 3D modeling, printing, illustrating on the weekend.
             </p>
-            <div class="flex flex-col mx-auto md:flex-row justify-center lg:justify-start text-center lg:text-center gap-6 md:gap-8 lg:gap-10">
-                <a class="blueShadow lg:mr-auto text-base sm:text-lg md:text-xl Rubik relative overflow-hidden px-6 py-3 group rounded-full bg-slate-950 text-white cursor-default">
+            <div class="flex flex-col mx-auto md:flex-row justify-center lg:justify-start text-center lg:text-center gap-4 md:gap-4 lg:gap-6">
+                <!-- <a class="blueShadow lg:mr-auto text-base sm:text-lg md:text-xl Rubik relative overflow-hidden px-6 py-3 group rounded-full bg-slate-950 text-white cursor-default">
                     <div class="absolute top-0 right-full w-full h-full">
                     </div>
                     <h4 class="relative z">UX/UI</h4>
-                </a>
+                </a> -->
+                
                 <a class="blueShadow lg:mr-auto text-base sm:text-lg md:text-lg Rubik relative overflow-hidden px-6 py-3 group rounded-full bg-slate-100 text-black cursor-pointer">
+                    <a class="blueShadow lg:mr-auto text-base sm:text-lg md:text-xl Rubik relative overflow-hidden px-6 py-3 group rounded-full bg-slate-950 text-white cursor-default">
+                        <div class="absolute top-0 right-full w-full h-full">
+                        </div>
+                        <h4 class="relative z">UX/UI</h4>
+                    </a>
                     <div class="absolute top-0 right-full w-full h-full bg-slate-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
                     </div>
                     <h4 class="relative z">Graphic Design</h4>
                 </a>
             </div>
-            <div class="px-4 py-4 max-w-sm overflow-hidden bg-slate-100 shadow-md rounded-md">
-                <img class="w-full rounded-md" src="project-card-lottie.png" alt="Project">
-                <div class="px-2 py-4">
-                <div class="font-bold text-xl text-left mb-2">The Coldest Sunset</div>
-            <p class="text-gray-700 text-base text-left">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-            </p>
-            </div>
-            <div class="px-6 pt-4 pb-2">
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+            <div class="flex flex-col mx-auto md:flex-row justify-center gap-6">
+                <div>
+                    <div class="px-4 py-4 max-w-sm overflow-hidden bg-white shadow-md rounded-md">
+                        <img class="w-full rounded-md" src="project-card-lottie.png" alt="Project">
+                        <div class="px-2 py-4">
+                        <div class="font-bold text-xl text-left mb-2">The Coldest Sunset</div>
+                    <p class="text-gray-700 text-base text-left">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                    </div>
+                        <div class="px-2 pt-4 pb-2">
+                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="px-4 py-4 max-w-sm overflow-hidden bg-white shadow-md rounded-md">
+                        <img class="w-full rounded-md" src="project-card-lottie.png" alt="Project">
+                        <div class="px-2 py-4">
+                        <div class="font-bold text-xl text-left mb-2">The Coldest Sunset</div>
+                    <p class="text-gray-700 text-base text-left">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                    </div>
+                        <div class="px-2 pt-4 pb-2">
+                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="px-4 py-4 max-w-sm overflow-hidden bg-white shadow-md rounded-md">
+                        <img class="w-full rounded-md" src="project-card-lottie.png" alt="Project">
+                        <div class="px-2 py-4">
+                        <div class="font-bold text-xl text-left mb-2">The Coldest Sunset</div>
+                    <p class="text-gray-700 text-base text-left">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                    </div>
+                        <div class="px-2 pt-4 pb-2">
+                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <div class="flex flex-col mx-auto md:flex-row justify-center lg:justify-start text-center lg:text-center gap-6 md:gap-8 lg:gap-10">
+            <a class="blueShadow lg:mr-auto text-base sm:text-lg md:text-lg Rubik relative overflow-hidden px-6 py-3 group rounded-full bg-slate-100 text-black cursor-pointer">
+                <div class="absolute top-0 right-full w-full h-full bg-slate-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
+                </div>
+                <h4 class="relative z">See More of My Work</h4>
+            </a>
+            </div>
+
+            <div class="px-2 pt-4 pb-2 gap-6">
+                <div class="inline-block lg:text-lg md:text-lg sm:text-md mb-2 mx-2">HTML</div>
+                <div class="inline-block lg:text-lg md:text-lg sm:text-md mb-2 mx-2">CSS</div>
+                <div class="inline-block lg:text-lg md:text-lg sm:text-md mb-2 mx-2">UX</div>
+            </div>
+
+            <section class>
+                <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+                    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center">Let's Connect!</h2>
+                    <form action="#" class="space-y-8">
+                        <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-left">Your email</label>
+                            <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@email.com" required>
+                        </div>
+                        <div>
+                            <label for="subject" class="block mb-2 text-sm text-left font-medium">Subject</label>
+                            <input type="text" id="subject" class="block p-3 w-full text-sm  bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required>
+                        </div>
+                        <div class="sm:col-span-2">
+                            <label for="message" class="block mb-2 text-sm text-left font-medium">Your message</label>
+                            <textarea id="message" rows="6" class="block p-2.5 w-full text-sm  bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+                        </div>
+                        <div class="flex flex-col mx-auto md:flex row justify-left lg:justify start text-center lg:text center gap-6 md:gap-8 lg:gap-10"> 
+                            <div class="blueShadow lg:mr-auto text-base sm:text-lg md:text-lg Rubik relative overflow-hidden px-6 py-3 group rounded-full bg-slate-100 text-black cursor-pointer">
+                            <div class="absolute top-0 right-full w-full h-full bg-blue-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
+                            </div> 
+                            <button type="submit">Send message</button>
+                        </div>
+                    </form>
+                    
+                    
+                </div>
+              </section>
+              
+
     </section>
 
 </main>
-
